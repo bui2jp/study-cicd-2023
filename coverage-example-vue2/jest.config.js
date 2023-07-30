644c125688ae6b05bb2ad372f763b26b17564d2b
bui2jp <chikutaku.reiwa.2020@gmail.com>
2023-07-30 09:38:58 +0900

    add ut, covarage

--- coverage-example-vue2/jest.config.js
+++ coverage-example-vue2/jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  "collectCoverage": true,
+  "collectCoverageFrom": ["**/*.{ts,js,vue}", "!**/node_modules/**"]
 };
